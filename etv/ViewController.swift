a2fca689769235312cd146782fac02abc9880fec
Priit Haamer <priit@fraktal.ee>
2015-09-10 09:34:10 +0300

    Initial Commit

---
+++ etv/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  etv
+//
+//  Created by Priit Haamer on 10.09.15.
+//  Copyright © 2015 Priit Haamer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
