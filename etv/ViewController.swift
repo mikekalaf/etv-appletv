40df17dd22479e5997d3d9117955555f0ffad376
Priit Haamer <priit@fraktal.ee>
2015-12-26 15:05:43 +0200

    Add ETV2

--- etv/ViewController.swift
+++ etv/ViewController.swift
@@ -11,17 +11,49 @@ import AVFoundation
 
 class ViewController: UIViewController {
 
-    var player = AVPlayer(URL: NSURL(string: "http://wowza3.err.ee/live/smil:etv.smil/playlist.m3u")!)
+    let etv = "http://wowza3.err.ee/live/smil:etv.smil/playlist.m3u"
+    
+    let etv2 = "http://wowza3.err.ee/live/smil:etv2.smil/playlist.m3u"
+    
+    var channel = "etv"
+    
+    var player = AVPlayer()
+    
+    var layer = AVPlayerLayer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let layer = AVPlayerLayer(player: player)
+        player = AVPlayer(URL: NSURL(string: etv)!)
         
+        layer.player = player
         layer.frame = self.view.frame
         layer.videoGravity = AVLayerVideoGravityResizeAspectFill
         
         self.view.layer.addSublayer(layer)
+    }
+    
+    override func pressesBegan(presses: Set<UIPress>, withEvent event: UIPressesEvent?) {
+        if (presses.first?.type == UIPressType.Menu) {
+            // handle event
+            player.pause()
+            
+            if (self.channel == "etv") {
+                player = AVPlayer(URL: NSURL(string: etv2)!)
+                self.channel = "etv2"
+            } else {
+                player = AVPlayer(URL: NSURL(string: etv)!)
+                self.channel = "etv"
+            }
+            
+            layer.player = player
+            layer.videoGravity = AVLayerVideoGravityResizeAspectFill
+            
+            player.play()
+        } else {
+            // perform default action (in your case, exit)
+            super.pressesBegan(presses, withEvent: event)
+        }
     }
     
     override func didReceiveMemoryWarning() {
